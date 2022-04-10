ac63736f63318b5c8e71c289f2dc3c3e126faf3c
Тимофеев Иван <itimofeev@stech.ru>
2022-04-11 02:44:15 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
+
+#[tokio::main]
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8181").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+    }
 }
